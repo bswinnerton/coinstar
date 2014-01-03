1217236286f172e2eac1db0d7fa22a9c3608e61a
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-03 01:17:42 -0500

    Requirement #1
    Make change from a given amount of cents and returns a
    currency set

--- lib/change_machine.rb
+++ lib/change_machine.rb
@@ -1,3 +1,31 @@
-class ChangeMachine
+require 'active_support/inflector'
 
+class ChangeMachine
+  CURRENCY = {penny: 1, nickel: 5, dime: 10, quarter: 25}
+
+  def self.make_change(cents)
+    calculated_change = sorted_currency.inject({}) do |change, (k,v)|
+      unless cents < v
+        divisible = cents / v
+        change[k] = divisible
+        cents = cents - (CURRENCY[k] * divisible)
+      end
+      change
+    end
+    pluralize(calculated_change)
+  end
+
+  private
+
+  def self.sorted_currency
+    Hash[CURRENCY.sort_by{|k, v| v}.reverse]
+  end
+
+  def self.pluralize(hash)
+    hash.inject({}) do |plural_hash, (k,v)|
+      plural_key = k.to_s.pluralize
+      plural_hash[plural_key.to_sym] = v
+      plural_hash
+    end
+  end
 end
--- spec/change_machine_spec.rb
+++ spec/change_machine_spec.rb
@@ -1,8 +1,33 @@
 require 'spec_helper'
 
 describe ChangeMachine do
-  it 'Makes change from a given amount of cents and returns a currency set'
-  it 'Makes change for quantities up to 100 cents'
-  it 'Makes cents from a given amount of currency and returns as a total number of cents'
-  it 'Takes input from the command line'
+  it 'sorts the currency by the largest denomination' do
+    expect(ChangeMachine::CURRENCY).to eq( {quarter: 25, dime: 10, nickel: 5, penny: 1} )
+  end
+
+  context 'Makes change from a given amount of cents and returns a currency set' do
+    it 'for 1' do
+      change = ChangeMachine.make_change(1)
+      expect(change).to eq( {pennies: 1} )
+    end
+
+    it 'for 16' do
+      change = ChangeMachine.make_change(16)
+      expect(change).to eq( {dimes: 1, nickels: 1, pennies: 1} )
+    end
+
+    it 'for 23' do
+      change = ChangeMachine.make_change(23)
+      expect(change).to eq( {dimes: 2, pennies: 3} )
+    end
+
+    it 'for 98' do
+      change = ChangeMachine.make_change(98)
+      expect(change).to eq( {quarters: 3, dimes: 2, pennies: 3} )
+    end
+  end
+
+  context 'Makes change for quantities up to 100 cents'
+  context 'Makes cents from a given amount of currency and returns as a total number of cents'
+  context 'Takes input from the command line'
 end
